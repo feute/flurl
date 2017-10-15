468efdb437f94f1dd945d3254fd9f72ff3bfc7b7
Angel Gomez <jangelgr7@gmail.com>
2017-10-15 11:30:54 -0400

    Add config for database

--- flurl/flurl.py
+++ flurl/flurl.py
@@ -3,5 +3,9 @@ from flask import Flask
 # Create the application instance.
 app = Flask(__name__)
 
+app.config.update(
+    DATABASE=os.path.join(app.root_path, 'flurl.db'),
+)
+
 # Additionally load config from the environment variable.
 app.config.from_envvar('FLURL_SETTINGS', silent=True)
