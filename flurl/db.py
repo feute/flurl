421e8a150e008634d54c79098d29f90770cd882a
Angel Gomez <jangelgr7@gmail.com>
2017-11-18 21:40:41 -0400

    Improve docstring styling according to PEP-257

--- flurl/db.py
+++ flurl/db.py
@@ -4,21 +4,16 @@ from flask import Blueprint, g, current_app
 
 
 def connect_db():
-    '''
-    Connect to the database specified in the config.
-    '''
-
+    """Connect to the database specified in the config."""
     rv = sqlite3.connect(current_app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
 
 
 def get_db():
-    '''
-    Open a new database connection if there is none yet for the
+    """Open a new database connection if there is none yet for the
     current application context.
-    '''
-
+    """
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
 
@@ -26,10 +21,7 @@ def get_db():
 
 
 def init_db():
-    '''
-    Initialise the database by reading the schema file.
-    '''
-
+    """Initialise the database by reading the schema file."""
     db = get_db()
 
     with current_app.open_resource('schema.sql', mode='r') as f:
@@ -39,10 +31,7 @@ def init_db():
 
 
 def save_url(original, short):
-    '''
-    Save the specified URL to the database
-    '''
-
+    """Save the specified URL to the database."""
     db = get_db()
     db.execute('insert into urls (short, original) values (?, ?)',
                 [short, original])
@@ -50,11 +39,9 @@ def save_url(original, short):
 
 
 def fetch_url(short):
-    '''
-    Fetch the shortened URL from the database and return the original
+    """Fetch the shortened URL from the database and return the original
     URL.
-    '''
-
+    """
     db = get_db()
     cur = db.execute('select original from urls where short=? limit 1',
                       [short])
--- flurl/flurl.py
+++ flurl/flurl.py
@@ -7,20 +7,18 @@ from secrets import token_urlsafe
 from .db import init_db, save_url, fetch_url
 
 
-usage = '''flurl is a simple URL shortener.
+usage = """flurl is a simple URL shortener.
 
 It is as simple as posting your URL to / and getting your shortened URL
 as the response.
 
 usage: curl -X POST -d <your-url> http://<server-url>/
-'''
+"""
 
 def create_app(config=None):
-    '''
-    Application factory to create the application with the ability
-    to provide additional configuration from the config argument.
-    '''
-
+    """Application factory to create the application with the ability to
+    provide additional configuration from the config argument.
+    """
     app = Flask(__name__)
 
     app.config.update(dict(
@@ -41,11 +39,10 @@ def create_app(config=None):
 def register_cli(app):
     @app.cli.command('initdb')
     def initdb_command():
-        '''
-        CLI command to initialise the database, usage:
+        """CLI command to initialise the database.
+        Usage:
             $ flask initdb
-        '''
-
+        """
         init_db()
         print('Initialised the database.')
 
@@ -53,10 +50,7 @@ def register_cli(app):
 def register_teardowns(app):
     @app.teardown_appcontext
     def close_db(error):
-        '''
-        Close the database at the end of the request.
-        '''
-
+        """Close the database at the end of the request."""
         if hasattr(g, 'sqlite_db'):
             g.sqlite_db.close()
 
@@ -85,15 +79,12 @@ def register_routes(app):
     def redirect_url(short_url):
         original_url = fetch_url(short_url)
         if not original_url:
-            return 'The URL doesn\'t exist.\n'
+            return "The URL doesn't exist.\n"
 
         return redirect(original_url)
 
 
 def generate_url(length=4):
-    '''
-    Generate a random URL string with a default length of 4.
-    '''
-
+    """Generate a random URL string with a default length of 4."""
     shortened = token_urlsafe(length)
     return shortened
